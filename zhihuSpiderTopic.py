38fdd62009ad0779906ff46a7a7f6eb844261b3e
wanglei03 <397179459@qq.com>
2021-05-09 03:33:39 +0800

    add思路

--- zhihuSpiderTopic.py
+++ zhihuSpiderTopic.py
@@ -1,6 +1,6 @@
 # @Author: wanglei03
-# @Time: 2021-5-6 21:25:19
-# @Method: 实现指定问题和答案的ID爬取图片
+# @Time: 2021-5-7 13:18:58
+# @Method: 实现指定话题的ID爬取图片
 
 import json
 import logging
@@ -11,12 +11,25 @@ import socket
 import random
 import threadpool
 from bs4 import BeautifulSoup
-import time
 
 logging.basicConfig(level=logging.NOTSET)
 socket.setdefaulttimeout(60)
-# 2021-5-9 01:53:54 保存文件夹时增加当前的时间戳
-NOW_TIME = time.strftime('%Y%m%d%H%M',time.localtime())
+
+'''
+通过问题的 URL 获取 json 内容. limit 是每次获取的增量，offset是浏览的起始值，
+因为后面是按照逐个回答建立文件夹，所以此处的 limit 建议设置为1，
+sortWay是排序方式，默认是按照热度排序，如果想按照时间排序，传参的时候需要指定sortWay是 created
+'''
+
+def url_topic(topicid_, limit_=1, offset_=0):
+    pass
+
+
+def url_question(questionId, limit=1, offset=0, sortWay='default'):
+    question_url = 'https://www.zhihu.com/api/v4/questions/' + str(questionId) + \
+                   '/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics%3Bdata%5B%2A%5D.settings.table_of_content.enabled' \
+                   '&limit=' + str(limit) + '&offset=' + str(offset) + '&platform=desktop&sort_by=' + sortWay
+    return url_json(question_url)
 
 
 # 通过 请求url 返回json内容
@@ -48,21 +61,7 @@ def url_json(myUrl):
         return json.loads(f.read().decode('utf-8'))
 
 
-'''
-通过问题的 URL 获取 json 内容. limit 是每次获取的增量，offset是浏览的起始值，
-因为后面是按照逐个回答建立文件夹，所以此处的 limit 建议设置为1，
-sortWay是排序方式，默认是按照热度排序，如果想按照时间排序，传参的时候需要指定sortWay是 created
-'''
-def url_question(questionId, limit=1, offset=0, sortWay='default'):
-    question_url = 'https://www.zhihu.com/api/v4/questions/' + str(questionId) + \
-                   '/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics%3Bdata%5B%2A%5D.settings.table_of_content.enabled' \
-                   '&limit=' + str(limit) + '&offset=' + str(offset) + '&platform=desktop&sort_by=' + sortWay
-    return url_json(question_url)
-
-
-'''
-@ 2021-5-9 02:42:57此方法已作废，找图都改为bf解析
-通过正则找到json内容中的图片地址，原图是保存在属性 data-original 中
+# 通过正则找到json内容中的图片地址，原图是保存在属性 data-original 中
 def re_get_img_url(myJsonText):
     pattern = re.compile('data-original="(.*?)"')
     img_url_list_re = pattern.findall(str(myJsonText))
@@ -70,20 +69,22 @@ def re_get_img_url(myJsonText):
     logging.info(
         '=====================第 {} 个答案用 正则 解析到 {} 张图片====================='.format(j + 1, len(img_url_only_list)))
     return img_url_only_list
-'''
 
 
-'''
-2021-5-9 02:43:34 重大更新
-测试的时候发现其实获取json中的content内容后，也是可以解析成soup对象的，那么就代表可以跟解析指定答案的HTML一样，用bf获取图片地址，
-而且还可以把这一模块抽取出来，便于代码复用
-'''
-def soup_img_url(myContent):
-    soup = BeautifulSoup(myContent, 'html.parser')
+# 通过答案的 URL 用 BeautifulSoup4 获取图片
+def url_answer(questionId, answerId):
+    answer_url = 'https://www.zhihu.com/question/' + str(questionId) + '/answer/' + str(answerId)
+    request = urllib.request.Request(answer_url)
+    response = urllib.request.urlopen(request)
+    content = response.read()
+    soup = BeautifulSoup(content, 'html.parser')
     try:
-        if j == -1:
-            question_name_s = soup.find(class_="QuestionHeader-title").get_text()  # 问题中文名
-            writer_name_s = soup.find(itemprop="author").meta['content']  # 作者
+        question_name = soup.find(class_="QuestionHeader-title").get_text()  # 问题中文名
+        writer_name = soup.find(itemprop="author").meta['content']  # 作者
+        global QUESTION_CHINESE
+        global AUTHOR_CHINESE
+        QUESTION_CHINESE = re_only_chinese(question_name)
+        AUTHOR_CHINESE = re_only_chinese(writer_name)
         img_url_item = soup.find_all('img', class_="origin_image zh-lightbox-thumb lazy")
         img_url_list_bf = []
         for x in img_url_item:
@@ -97,32 +98,11 @@ def soup_img_url(myContent):
         img_url_item_o = soup.find_all('img', class_='content_image lazy')
         for y in img_url_item_o:
             img_url_list_bf.append(y.get('data-actualsrc'))
-        logging.info(
-            '=====================第 {} 个答案用 bf4 解析到 {} 张图片====================='.format(j + 1, len(img_url_list_bf)))
-        if j == -1:
-            return img_url_list_bf, question_name_s, writer_name_s
-        else:
-            return img_url_list_bf
+        logging.info('用 beautifulSoup4 解析到 {} 张图片'.format(len(img_url_list_bf)))
+        return img_url_list_bf
     except:
         logging.error('未找到任何图片，点击 https://www.zhihu.com/question/{}/answer/{} 检查路径'.format(question_id, answer_id))
         os._exit(-1)
-
-
-# 通过答案的 URL 用 BeautifulSoup4 获取图片
-def url_answer(questionId, answerId):
-    answer_url = 'https://www.zhihu.com/question/' + str(questionId) + '/answer/' + str(answerId)
-    request = urllib.request.Request(answer_url)
-    response = urllib.request.urlopen(request)
-    content = response.read()
-    # 保证只运行一次，否则会多次输出log信息
-    info_list = soup_img_url(content)
-    question_name = info_list[1]
-    writer_name = info_list[2]
-    global QUESTION_CHINESE
-    global AUTHOR_CHINESE
-    QUESTION_CHINESE = re_only_chinese(question_name)
-    AUTHOR_CHINESE = re_only_chinese(writer_name)
-    return info_list[0]
 
 
 # 根据图片地址下载图片，这里的 i 主要是用来文件命名
@@ -138,6 +118,14 @@ def download_pic(i, imgUrl):
         logging.info('第 {} 张下载完成 {}'.format(i + 1, file_name))
     except:
         logging.warning("链接失效，图片无法下载")
+
+
+# 从字符串中只匹配中文
+def re_only_chinese(s):
+    pattern = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
+    ret = pattern.sub("", s)
+    # 虽然文件名长度限制长达200多，但是为了易读性，我们还是设置一下截取的长度，毕竟已经有问题和答案ID
+    return ret[:20]
 
 
 # 多线程下载列表中的url
@@ -160,15 +148,6 @@ def thread_pool_download(imgUrlList):
     logging.info('保存路径为   {}'.format(absolute_dir))
 
 
-# 从字符串中只匹配中文
-def re_only_chinese(s):
-    pattern = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
-    ret = pattern.sub("", s)
-    # 虽然文件名长度限制长达200多，但是为了易读性，我们还是设置一下截取的长度，毕竟已经有问题和答案ID
-    return ret[:20]
-
-
-
 if __name__ == '__main__':
     # 采用问题和答案 ID 下载，必须都输入
     question_id = ''
@@ -176,19 +155,17 @@ if __name__ == '__main__':
     # 问题ID列表，多份快乐，如果不想单独设置每个回答下的下载数量， 修改 want_answer_num = answer_counts 即为下载全部
     # question_id_l = [366062253, 338323696, 424555505, 350939352]  # 我乱打的几个ID，兄弟们自己不要当真（狗头）
     question_id_l = [316722332, 397912593, ]
-    # question_id_l = []
+
     # 判断用户是否输入了 question_id 和 answer id， 如果输入了，就采用 BeautifulSoup4 的方法解析 html ，而非解析json
     if question_id and answer_id:
         logging.info(
             '正在使用 beautifulSoup4 方式解析 https://www.zhihu.com/question/{}/answer/{}'.format(question_id, answer_id))
-        # 因为 soup_img_url 函数在回答中要用到循环，所以这里指定一个j，也相当于一个 flag 标志位
-        j = -1
         img_url_list = url_answer(question_id, answer_id)
         # 判断 BeautifulSoup4 解析之后的列表，基本上是不可能为空的，因为是直接通过answerid的，如果为空，提示用户检查此答案下是否有图片
         if img_url_list:
             # 这几步都是创建路径
             path = os.getcwd()
-            new_path = str(path) + '-qid-' + question_id + '-' + QUESTION_CHINESE + '-' +NOW_TIME
+            new_path = str(path) + '-qid-' + question_id + '-' + QUESTION_CHINESE
             absolute_dir = new_path + '\\' + 'aid-{}-{}'.format(answer_id, AUTHOR_CHINESE)
             # 因为下面用到了 download_pic，函数，所以这里必须定义成文件夹形式保存
             folder_or_file = 1
@@ -203,36 +180,36 @@ if __name__ == '__main__':
         else:
             logging.warning(
                 '此答案下没有任何图片，点击 https://www.zhihu.com/question/{}/answer/{} 查看'.format(question_id, answer_id))
+
     elif question_id_l:
         for question_id in question_id_l:
             question_id = str(question_id)
-            logging.info('正在使用 json + bf 方式解析 https://www.zhihu.com/question/{}'.format(question_id))
+            logging.info('正在使用 json + re 方式解析 https://www.zhihu.com/question/{}'.format(question_id))
             json_text_tmp = url_question(question_id)
             answer_counts = json_text_tmp['paging']['totals']  # 答案总数量
             question_name = json_text_tmp['data'][0]['question']['title']  # 问题中文名
-            logging.info('Start....@@@@@@@@@@@@@@ 问题 @@@@@@@@@@@ {} 下有 # {} 个回答'.format(question_name, answer_counts))
+            logging.info('@@@@@@@@@@@@@@ 问题 @@@@@@@@@@@ {} 下有 # {} 个回答'.format(question_name, answer_counts))
             '''
             前多少个回答，默认是按照热度排序，一般设置成 50 或者 100，依照答案数量而定，
             一般也就前面点赞的还可以，群众的眼睛是雪亮的，私认为热度前 100 足够了
             现在知乎老是有整流大师，搜集各种回答，所以前几名一般都是他们，不建议设置前 5
             '''
             # want_answer_num = int(input("请输入需要下载前多少个答案的数量（不输入默认 100 ）：") or 100)
-            want_answer_num = 5  # 测试使用
+            want_answer_num = 4  # 测试使用
             # want_answer_num = answer_counts   # 不想单独选的，每个问题全部下载，但是会特别费时，一般福利问题都几百上千个回答
             for j in range(want_answer_num):
                 json_text = url_question(questionId=question_id, offset=j)
-                answer_content = json_text['data'][0]['content']
-                # img_url_list_tmp = re_get_img_url(answer_text)  # 废弃的正则找图方法
-                img_url_list_tmp = soup_img_url(answer_content)
+                answer_text = json_text['data'][0]['content']
+                img_url_list_tmp = re_get_img_url(answer_text)
                 my_answer_id = json_text['data'][0]['id']  # 答案id
                 author = json_text['data'][0]['author']['name']  # 作者
                 if img_url_list_tmp:
                     question_chinese = re_only_chinese(question_name)
                     author_chinese = re_only_chinese(author)
                     path = os.getcwd()
-                    new_path = str(path) + '-qid-' + question_id + '-' + question_chinese + '-' +NOW_TIME
+                    new_path = str(path) + '-qid-' + question_id + '-' + question_chinese
                     # 选择是否保存成文件夹  1  还是 按照一个文件下不同文件名区分  0
-                    folder_or_file = 0
+                    folder_or_file = 1
                     if folder_or_file:
                         absolute_dir = new_path + '\\' + '{}-aid-{}-{}'.format(j + 1, my_answer_id, author_chinese)
                     else:
@@ -250,7 +227,12 @@ if __name__ == '__main__':
                 end_flag = json_text['paging']['is_end']
                 if end_flag:
                     break
-            logging.info('Done....############# 问题 ########### {} 下载完毕'.format(question_name))
+            logging.info('############# 问题 ########### {} 下载完毕'.format(question_name))
     else:
         logging.warning('请检查  question_id, answer_id, question_id_l 参数是否正确输入')
+
     logging.info('!!!!!!!!!!!!!  执行完毕，谢谢使用  !!!!!!!!!!!!!!!')
+
+    # t1 = time.time()
+    # totaltime = t1 - t0
+    # print("use time : ", totaltime)
